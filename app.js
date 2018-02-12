7747eb121275b3c292b322d112a1886fd82205eb
Your Name <youremail@domain.com>
2018-02-12 00:16:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
+//app.listen(process.env.PORT, process.env.IP, function() {
+  //  console.log("Server is up!");
+//})
